61db78b8b4d8a1bf793b10b1f59fd2311276e43a
Athanasios Oikonomou <athoik@gmail.com>
2016-11-21 18:10:10 +0200

    Fix pkg in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
+pkg = 'Enigma2 Software Defined Radio'
 setup (name = 'enigma2-plugin-extensions-sdgradio',
        version = '1.0',
        description = 'Enigma2 Software Defined Radio',
        package_dir = {pkg: 'plugin'},
-       packages = ['Extensions.SDGRadio'],
+       packages = [pkg],
        package_data = {pkg: ['fonts/*.ttf', 'img/*.png']},
       )
 
