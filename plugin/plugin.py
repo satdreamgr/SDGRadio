d2ade1f616d76a3b54d877cf59598b00b0627946
Athanasios Oikonomou <athoik@gmail.com>
2018-01-11 22:55:20 +0200

    Add bitrate and dab type in program info

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -270,7 +270,8 @@ class SDGRadioScreen(Screen):
 			if "prog_type" in rds and self["prog_type"].getText() != rds["prog_type"].encode('utf8'):
 				self["prog_type"].setText(rds["prog_type"].encode('utf8'))
 			if "programType" in rds:
-				self["prog_type"].setText(rds["programType"].encode('utf8'))
+				txt = u"%s kbps %s %s" % (rds["bitrate"], rds["dabType"], rds["programType"])
+				self["prog_type"].setText(txt.encode('utf8'))
 			if "programName" in rds and "programId" in rds:
 				self.programs.append((rds["programName"].encode('utf8'), rds["programId"]))
 		except Exception as e:
