e676183fb485180a20ce509ce3755f97d6265e29
nautilus7 <spyros.n7@gmail.com>
2019-12-27 02:32:02 +0200

    Fixed package building

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
+import setup_translate
 
 pkg = 'Extensions.SDGRadio'
 setup (name = 'enigma2-plugin-extensions-sdgradio',
-       version = '1.0',
+       version = '2.0',
        description = 'Enigma2 Software Defined Radio',
        package_dir = {pkg: 'plugin'},
        packages = [pkg],
        package_data = {pkg: ['fonts/*.ttf', 'img/*.png']},
-      )
-
+       cmdclass=setup_translate.cmdclass,
+)
