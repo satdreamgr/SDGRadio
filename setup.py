dc216cd5126fa6acdf90c7c5b9706530fdd420ae
persianpros <persianpros@yahoo.com>
2021-04-13 23:00:19 +0430

    PEP8 double aggressive E251 and E252

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 import setup_translate
 
 pkg = 'Extensions.SDGRadio'
-setup (name = 'enigma2-plugin-extensions-sdgradio',
-       version = '2.0',
-       description = 'Enigma2 Software Defined Radio',
-       package_dir = {pkg: 'plugin'},
-       packages = [pkg],
-       package_data = {pkg: ['*.xml', '*.png', 'fonts/*.ttf', 'img/*.png']},
-       cmdclass = setup_translate.cmdclass,
+setup (name='enigma2-plugin-extensions-sdgradio',
+       version='2.0',
+       description='Enigma2 Software Defined Radio',
+       package_dir={pkg: 'plugin'},
+       packages=[pkg],
+       package_data={pkg: ['*.xml', '*.png', 'fonts/*.ttf', 'img/*.png']},
+       cmdclass=setup_translate.cmdclass,
 )
